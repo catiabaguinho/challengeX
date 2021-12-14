f531f6ccb1ba6a628a50e15c3bcecdcc0f2906cb
catiabaguinho <48532244+catiabaguinho@users.noreply.github.com>
2021-12-13 18:14:34 -0600

    actualización

--- python/code/load_data.py
+++ python/code/load_data.py
@@ -14,43 +14,57 @@ while True:
     except socket.error as ex:
         time.sleep(0.1)
 
+try:
+    conn = psycopg2.connect("host='db' port='5432' dbname='test_db' user='pgadmin' password='Abc.1234%'")
+    cur = conn.cursor()
+except Exception as err:
+        print ("Error conectado a BD", err)
 
-conn = psycopg2.connect("host='db' port='5432' dbname='test_db' user='pgadmin' password='Abc.1234%'")
-cur = conn.cursor()
 
-cur.execute("""
+try:
+    cur.execute("""
 
-CREATE TABLE IF NOT EXISTS public.personas
-(
-    id uuid NOT NULL DEFAULT uuid_generate_v4(),
-    first_name character varying(50) COLLATE pg_catalog."default",
-    last_name character varying(50) COLLATE pg_catalog."default",
-    company_name character varying(50) COLLATE pg_catalog."default",
-    address character varying(50) COLLATE pg_catalog."default",
-    city character varying(50) COLLATE pg_catalog."default",
-    state character(2) COLLATE pg_catalog."default",
-    zip character varying(10) COLLATE pg_catalog."default",
-    phone1 character varying(15) COLLATE pg_catalog."default",
-    phone2 character varying(15) COLLATE pg_catalog."default",
-    email character varying(50) COLLATE pg_catalog."default",
-    department character varying(50) COLLATE pg_catalog."default",
-    CONSTRAINT personas_pkey PRIMARY KEY (id)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.personas
-    OWNER to pgadmin;""")
-
-cur.execute("""TRUNCATE TABLE public.personas;""")
-
-with open('/usr/rawdata/Sample.csv', 'r') as f:
-    reader = csv.reader(f)
-    next(reader) # Skip the header row.
-    for row in reader:
-        cur.execute("INSERT INTO public.personas  (first_name, last_name, company_name, address,city, state,zip, phone1, phone2, email, department) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);",
-        row
+    CREATE TABLE IF NOT EXISTS public.personas
+    (
+        id uuid NOT NULL DEFAULT uuid_generate_v4(),
+        first_name character varying(50) COLLATE pg_catalog."default",
+        last_name character varying(50) COLLATE pg_catalog."default",
+        company_name character varying(50) COLLATE pg_catalog."default",
+        address character varying(50) COLLATE pg_catalog."default",
+        city character varying(50) COLLATE pg_catalog."default",
+        state character(2) COLLATE pg_catalog."default",
+        zip character varying(10) COLLATE pg_catalog."default",
+        phone1 character varying(15) COLLATE pg_catalog."default",
+        phone2 character varying(15) COLLATE pg_catalog."default",
+        email character varying(50) COLLATE pg_catalog."default",
+        department character varying(50) COLLATE pg_catalog."default",
+        CONSTRAINT personas_pkey PRIMARY KEY (id)
     )
-conn.commit()
+
+    TABLESPACE pg_default;
+
+    ALTER TABLE public.personas
+        OWNER to pgadmin;""")
+
+except Exception as err:
+        print ("Error creando la tabla", err)
+
+try:
+    cur.execute("""TRUNCATE TABLE public.personas;""")
+except Exception as err:
+        print ("Error limpiando la tabla", err)
+
+
+try:      
+    with open('/usr/rawdata/Sample.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader) # Skip the header row.
+        for row in reader:
+            cur.execute("INSERT INTO public.personas  (first_name, last_name, company_name, address,city, state,zip, phone1, phone2, email, department) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);",
+            row
+        )
+    conn.commit()
+except Exception as err:
+        print ("Error cargando los datos en la tabla", err)
 
 print("Sucess!!")
